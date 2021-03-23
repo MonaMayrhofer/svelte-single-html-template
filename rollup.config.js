12b832f4bd5282c55b51335c91cf2076f8560477
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2021-03-23 18:47:50 +0100

    single file svelte

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import svelte from "rollup-plugin-svelte";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import livereload from "rollup-plugin-livereload";
-import { terser } from "rollup-plugin-terser";
 import sveltePreprocess from "svelte-preprocess";
 import path from "path";
 import fs from "fs";
@@ -17,11 +16,10 @@ function inlineSvelte(templatePath) {
     generateBundle(opts, bundle) {
       const file = path.parse(opts.file).base;
       const jsCode = bundle[file].code;
-      const cssCode = bundle["bundle.css"].source;
+      //Rely on Svelte... const cssCode = bundle["bundle.css"].source;
       const template = fs.readFileSync(templatePath, "utf-8");
-      bundle[file].code = template
-        .replace("%%script%%", jsCode)
-        .replace("%%style%%", cssCode);
+      bundle[file].code = template.replace("%%script%%", jsCode);
+      //.replace("%%style%%", cssCode);
     },
   };
 }
@@ -57,10 +55,11 @@ export default {
     //sourcemap: true,
     //format: "iife",
     name: "app",
-    file: "public/build/index.html",
+    file: "dist/index.html",
   },
   plugins: [
     svelte({
+      emitCss: false,
       preprocess: sveltePreprocess({ sourceMap: !production }),
       compilerOptions: {
         // enable run-time checks when not in production
@@ -78,9 +77,9 @@ export default {
       inlineSources: !production,
     }),
     !production && serve(),
-    !production && livereload("public"),
+    !production && livereload("dist"),
     //production && terser(),
-    inlineSvelte("index.html"),
+    inlineSvelte("src/template.html"),
   ],
   watch: {
     clearScreen: false,
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+window.addEventListener("load", function () {
+  new App({
+    target: document.body,
+    props: {
+      name: "world",
+    },
+  });
 });
 
-export default app;
+//export default app;
